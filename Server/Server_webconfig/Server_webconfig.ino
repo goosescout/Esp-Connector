9c842833b49164aac3980a0c35222aa8e3a56d69
goosescout <michael.gurevich.02@mail.ru>
2021-03-23 18:31:31 +0700

    Joystick works

--- Server/Server_webconfig/Server_webconfig.ino
+++ Server/Server_webconfig/Server_webconfig.ino
@@ -8,6 +8,16 @@
 
 AsyncWebServer server(80);
 
+const int SW_pin = 17;
+const int X_pin = 35;
+const int Y_pin = 34;
+
+void getResponce(AsyncWebServerRequest *request) {
+    String responce;
+    responce = responce + analogRead(X_pin) + " " + analogRead(Y_pin);
+    request->send(200, "text/plain", responce);
+}
+
 void setup(void) {
     Serial.begin(115200);
     Serial.println("Starting ESP32 server...");
@@ -19,11 +29,14 @@ void setup(void) {
     Serial.print("AP IP address: ");
     Serial.println(IP);
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/plain", "success");
-    });
+    server.on("/", HTTP_GET, getResponce);
 
     server.begin();
+
+    pinMode(X_pin, INPUT);
+    pinMode(Y_pin, INPUT);
+    pinMode(SW_pin, INPUT_PULLUP);
+    digitalWrite(SW_pin, HIGH);
 }
 
 void loop(void) {
