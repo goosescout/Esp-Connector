5d96e518188b42cc030c136c13099dba3af18af2
goosescout <michael.gurevich.02@mail.ru>
2021-03-24 19:34:17 +0700

    Updating led

--- Client/Client_webconfig/Client_webconfig.ino
+++ Client/Client_webconfig/Client_webconfig.ino
@@ -13,6 +13,31 @@
 #define LED_COUNT 4
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN);
 
+void updateLED(int x, int y) {
+    if (x > 3000) {  // back
+        strip.setPixelColor(3, 255, 255, 255);
+    } else {
+        strip.setPixelColor(3, 0, 0, 0);
+    }
+    if (x < 1000) {  // forward
+        strip.setPixelColor(2, 255, 255, 255);
+    } else {
+        strip.setPixelColor(2, 0, 0, 0);
+    }
+    if (y > 3000) {  // right
+        strip.setPixelColor(1, 255, 255, 255);
+    } else {                
+        strip.setPixelColor(1, 0, 0, 0);
+    }
+    if (y < 1000) { // left
+        strip.setPixelColor(0, 255, 255, 255);
+    } else {
+        strip.setPixelColor(0, 0, 0, 0);
+    }
+    
+    strip.show();
+}
+
 void setup(void) {
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
@@ -52,8 +77,7 @@ void loop(void) {
                     y = y + symbol;
                 }
             }
-            x = x.toInt();
-            y = y.toInt();
+            updateLED(x.toInt(), y.toInt());
         } else {
             Serial.print("Error: ");
             Serial.println(httpCode);
@@ -61,6 +85,4 @@ void loop(void) {
         http.end();
     }
     delay(500);
-    strip.setPixelColor(2, 255, 255, 255);
-    strip.show();
 }
