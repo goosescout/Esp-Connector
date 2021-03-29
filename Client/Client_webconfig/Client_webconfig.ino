e65f07ca12c769617b52fcd42d7c78175d69ef9e
goosescout <michael.gurevich.02@mail.ru>
2021-03-29 20:32:07 +0700

    Updating device check
    TODO: change pins and joystick values

--- Client/Client_webconfig/Client_webconfig.ino
+++ Client/Client_webconfig/Client_webconfig.ino
@@ -9,19 +9,22 @@
 
 #define SERVER_ACCESS "http://192.168.4.1/"
 
-#define LED_PIN 9
+#define LED_PIN 2 // TODO: change to 9
 #define LED_COUNT 4
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN);
 
-#define ENGINE_PIN 10
+#define ENGINE_PIN 4 // TODO: change to 10
 #define SOLAR_ENABLE_PIN 15
 #define ANT_PIN 14
 
 #define SOLAR_ANALOG_PIN A0
 
 bool wifi_initialized = false;
+bool LED_initialized = false;
 
 void updateLED(int x, int y) {
+    Serial.println(x);
+    Serial.println(y);
     if (x > 2000) {  // back
         double norm = (x - 2000.0) / 2095.0 * 255.0;
         int int_norm = static_cast<int>(norm);
@@ -57,14 +60,19 @@ bool checkEngine() {
     bool engine_plugged = false;
     if (digitalRead(ENGINE_PIN) == LOW) {
         // инициализация светодиодов
-        strip.begin();
-        strip.setPixelColor(0, 0, 0, 0);
-        strip.setPixelColor(1, 0, 0, 0);
-        strip.setPixelColor(2, 0, 0, 0);
-        strip.setPixelColor(3, 0, 0, 0);
-        strip.show();
+        if (!LED_initialized) {
+            strip.setPixelColor(0, 0, 0, 0);
+            strip.setPixelColor(1, 0, 0, 0);
+            strip.setPixelColor(2, 0, 0, 0);
+            strip.setPixelColor(3, 0, 0, 0);
+            strip.show();
+            LED_initialized = true;
+            Serial.println("LED initialized");
+        }
 
         engine_plugged = true;
+    } else {
+        LED_initialized = false;
     }
     return engine_plugged;
 }
@@ -79,6 +87,7 @@ bool checkSolar() {
 }
 
 bool checkAnt() {
+    return true;
     bool ant_plugged = false;
     if (digitalRead(ANT_PIN) == LOW) {
         ant_plugged = true;
@@ -106,6 +115,13 @@ void wifiStop() {
     if (wifi_initialized) {
         WiFi.disconnect();
         wifi_initialized = false;
+        strip.setPixelColor(0, 0, 0, 0);
+        strip.setPixelColor(1, 0, 0, 0);
+        strip.setPixelColor(2, 0, 0, 0);
+        strip.setPixelColor(3, 0, 0, 0);
+        strip.show();
+        LED_initialized = false;
+        Serial.println("Wifi and LED disabled");
     }
 }
 
@@ -116,10 +132,13 @@ void setup(void) {
     pinMode(ENGINE_PIN, INPUT_PULLUP);
     pinMode(SOLAR_ENABLE_PIN, INPUT_PULLUP);
     pinMode(ANT_PIN, INPUT_PULLUP);
+    pinMode(SOLAR_ANALOG_PIN, INPUT_PULLUP);
+
+    strip.begin();
 }
 
 void loop(void) {
-    if (checkAnt() && checkEngine() && checkSolar()) {
+    if (checkEngine() && checkAnt() && checkSolar()) {
         wifiStart();
         if (WiFi.status() == WL_CONNECTED) {
             WiFiClient client;
